496b2d0a244e947b7a4b0227f82b06659f989c69
kals <bhavayami.apps03@gmail.com>
2026-05-01 16:03:03 +0530

    updated network worker to remove shell command

--- app/src/main/java/com/entertainment/kurtineck/deignss/NetworkWorker.kt
+++ app/src/main/java/com/entertainment/kurtineck/deignss/NetworkWorker.kt
@@ -1,74 +1,67 @@
 package com.entertainment.kurtineck.deignss
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.util.Log
 import kotlinx.coroutines.*
 
-
 object NetworkWorker {
 
-    private val  mSleepDurationConnected:Int = 60
-    private val  mSleepDurationDisconnected:Int = 10
-    var isOnline:Boolean = true
-    var adLimitEnabled: Boolean =false
+    private val mSleepDurationConnected: Int = 60
+    private val mSleepDurationDisconnected: Int = 10
+    var isOnline: Boolean = true
+    var adLimitEnabled: Boolean = false
 
+    lateinit var appContext: Context
 
-
-    fun runNetworkCheckingThread(){
-            val scope = CoroutineScope(Dispatchers.Default)
-            scope.launch {
-                try {
-                    while (!adLimitEnabled){
-                          isOnline=isDeviceOnline()
-                          logTheDeviceStatus()
-                          sleepForSomeTime(isOnline)
-                    }
-                } catch (throwable: Throwable) {
-                    Log.e("error",throwable.printStackTrace().toString())
-
+    fun runNetworkCheckingThread() {
+        val scope = CoroutineScope(Dispatchers.Default)
+        scope.launch {
+            try {
+                while (!adLimitEnabled) {
+                    isOnline = isDeviceOnline()
+                    logTheDeviceStatus()
+                    sleepForSomeTime(isOnline)
                 }
+            } catch (throwable: Throwable) {
+                Log.e("error", throwable.printStackTrace().toString())
             }
-
         }
+    }
 
     private fun logTheDeviceStatus() {
         Log.d("NetworkStatus", isOnline.toString())
     }
 
     private fun sleepForSomeTime(networkStatus: Boolean) {
-            try {
-                if (networkStatus) sleepUsingCoroutines(mSleepDurationConnected)
-                else sleepUsingCoroutines(mSleepDurationDisconnected)
-            }catch (ex:Exception){
-                Log.e("error", ex.printStackTrace().toString())
-            }
+        try {
+            if (networkStatus) sleepUsingCoroutines(mSleepDurationConnected)
+            else sleepUsingCoroutines(mSleepDurationDisconnected)
+        } catch (ex: Exception) {
+            Log.e("error", ex.printStackTrace().toString())
         }
+    }
 
-        private fun sleepUsingCoroutines(timeinSec:Int) {
-            try {
-                runBlocking { delay(timeinSec * 1000L) }
-            }catch (ex:Exception){
-                Log.e("ex",ex.printStackTrace().toString())
-            }
+    private fun sleepUsingCoroutines(timeinSec: Int) {
+        try {
+            runBlocking { delay(timeinSec * 1000L) }
+        } catch (ex: Exception) {
+            Log.e("ex", ex.printStackTrace().toString())
         }
+    }
 
-         fun  isDeviceOnline():Boolean {
-/*
-            if (!AdObject.isNetworkAvailable()){
-                return false
-            }
-*/
-            try {
-                /*Pinging to Google server*/
-                val command = "ping -c 1 google.com"
-                val status = Runtime.getRuntime().exec(command).waitFor() == 0
-                return status
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }  finally {
-            }
-            return false
+    fun isDeviceOnline(): Boolean {
+        return try {
+            val connectivityManager =
+                appContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val network = connectivityManager.activeNetwork ?: return false
+            val capabilities =
+                connectivityManager.getNetworkCapabilities(network) ?: return false
+            capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            false
         }
-
-
-
+    }
 }
